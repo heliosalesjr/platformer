759e34fc202ab2abc92de63d4703086e02933999
heliosalesjr <heliosalesjr@gmail.com>
2023-07-16 20:01:46 -0300

    added coyote jump to the player character

--- play2.gd
+++ play2.gd
@@ -6,13 +6,16 @@ const FRICTION = 1000
 const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 func _physics_process(delta):
+	
 	apply_gravity(delta)
+	
 	handle_jump()
 
 	var input_axis = Input.get_axis("ui_left", "ui_right")
@@ -23,9 +26,14 @@ func _physics_process(delta):
 	
 	update_animations(input_axis)
 	
+	var was_on_floor = is_on_floor()
+	
 	move_and_slide()
 	
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	
+	if just_left_ledge:
+		coyote_jump_timer.start()
 	
 
 func apply_gravity(delta):
@@ -33,10 +41,10 @@ func apply_gravity(delta):
 		velocity.y += gravity * delta
 		
 func handle_jump():
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY /2 
 
